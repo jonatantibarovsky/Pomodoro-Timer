5e4f2000df6ca1c79272b656871988ab9c7f730c
Jonatan Tibarovsky <jonatantibarovsky@gmail.com>
2019-09-24 16:12:33 +0200

    Updates

---
+++ precache-manifest.254c7dacc40064a291f9cb9abb1357b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302af07ae3d35d6d8981cfbd32bb638d",
+    "url": "/Pomodoro-Timer/index.html"
+  },
+  {
+    "revision": "6023e529c2eb246de1b3",
+    "url": "/Pomodoro-Timer/static/css/main.6998bcaa.chunk.css"
+  },
+  {
+    "revision": "92c24da17c84b3fd91ea",
+    "url": "/Pomodoro-Timer/static/js/2.b7c6ae21.chunk.js"
+  },
+  {
+    "revision": "6023e529c2eb246de1b3",
+    "url": "/Pomodoro-Timer/static/js/main.6c425d86.chunk.js"
+  },
+  {
+    "revision": "783f59404c6ea00c8016",
+    "url": "/Pomodoro-Timer/static/js/runtime-main.d4b3aee2.js"
+  }
+]);
